e692aa72db05ae8cc9765536f6e4eeb1d689a46f
harky0000 <moldoboss27@gmail.com>
2026-02-01 15:31:36 -0800

    Add Unpack lagger.lua for firing server events

---
+++ Unpack lagger.lua
@@ -0,0 +1,10 @@
+--crazy lagger
+
+for i = 1, 50 do
+local args = {
+    [1] = "Roblox",
+    [2] = CFrame.new(-14.05797004699707, 3, 9.9130859375, 1, -1.0901437974553119e-07, -7.840890263455247e-12, 1.0901437974553119e-07, 1, 3.4854056707445125e-07, 7.802893747799189e-12, -3.4854056707445125e-07, 1)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("THE_SOLDIER"):FireServer(unpack(args))
+end
